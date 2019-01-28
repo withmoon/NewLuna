aaee008e79a5003f411f131adec655e3f1211ae2
peb75 <peb75@DESKTOP-GPI5MRS>
2019-01-28 11:49:03 +0900

    아디/비번 찾기 화면

---
+++ Luna/src/main/java/com/study/luna/pub/controller/FindAccountController.java
@@ -0,0 +1,13 @@
+package com.study.luna.pub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FindAccountController {
+	@RequestMapping(value="/findaccount.do", method=RequestMethod.GET)
+	public String mainView() {
+		return "findaccount";
+	}
+}
