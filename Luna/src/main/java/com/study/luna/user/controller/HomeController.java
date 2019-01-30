6ce138fbd486cd28ac87f1f33d0378bd456eee3c
saeah <saeah@DESKTOP-6JGCCS1>
2019-01-30 17:03:57 +0900

    암호화 적용 완료 이제 세션 적용 들어갑니다.

--- Luna/src/main/java/com/study/luna/user/controller/HomeController.java
+++ Luna/src/main/java/com/study/luna/user/controller/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.study.luna.pub.command.MemberCommand;
 import com.study.luna.pub.member.service.MemberService;
+import com.study.luna.util.BCrypt;
+import com.study.luna.util.SHA256;
 
 @Controller
 public class HomeController {
@@ -26,17 +28,29 @@ public class HomeController {
 			memcom.setKid(kid);
 			memcom.setPw(knic);
 		}
-		
 		memcom.setBranchAddr2(memcom.getBranchAddr2()+"#"+memcom.getBranchAddr3());
 		
-		//값 넘김 테스트 js 확인용
-		System.out.println(memcom.getZipcode());
-		System.out.println(memcom.getBranchAddr1());
-		System.out.println(memcom.getBranchAddr2());
-		System.out.println(memcom.getBranchName());
+		SHA256 sha=SHA256.getInsatnce();
+		String shaPass="";
+		String bcPass="";
+		try {
+			shaPass = sha.getSha256(memcom.getPw().getBytes());
+			bcPass=BCrypt.hashpw(shaPass, BCrypt.gensalt());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		memcom.setPw(bcPass);
 		
 		memser.insertMember(memcom);
 		
 		return "home";
 	}
 }
+/*
+//값 넘김 테스트 js 확인용
+System.out.println(memcom.getZipcode());
+System.out.println(memcom.getBranchAddr1());
+System.out.println(memcom.getBranchAddr2());
+System.out.println(memcom.getBranchName());
+*/
