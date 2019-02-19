dd6bd20760d2b3cc4a1b1ea89a5baf23a065953d
saeah <saeah@DESKTOP-6JGCCS1>
2019-02-19 12:02:02 +0900

    마이바티스 커밋

---
+++ Luna/src/main/java/com/study/luna/user/dto/RoomReviewDTO.java
@@ -0,0 +1,11 @@
+package com.study.luna.user.dto;
+
+import java.sql.Date;
+
+public class RoomReviewDTO {
+	 int roomNum;
+     String reviewContent;
+     String name;
+     String id;
+     Date writedate;
+}
