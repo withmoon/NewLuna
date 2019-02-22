4fbadf72c5ab9f1f9f6e643a8be53a9c9315d758
alfo2-11 <alfo2-11@alfo2-11-PC>
2019-02-22 17:44:32 +0900

    커밋

--- Luna/src/main/java/com/study/luna/mg/controller/ManagerMainController.java
+++ Luna/src/main/java/com/study/luna/mg/controller/ManagerMainController.java
@@ -1,10 +1,15 @@
 package com.study.luna.mg.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.study.luna.mg.service.managerService;
 
@@ -16,11 +21,21 @@ public class ManagerMainController {
 	private managerService managerService; 
 	
 	@RequestMapping(value="/manager.mdo", method=RequestMethod.GET)
-	public ModelAndView mainView() {
+	public ModelAndView mainView(HttpServletRequest request) {
+		/*System.out.println("매니저 화면 호출 로그인 인증");
+		System.out.println(request.getParameter("id")+"님");
+		if(request.getParameter("id")==null || request.getParameter("id").equals("")) {
+			System.out.println("매니저 인증 실패");
+			return logincheck();
+		}else {
+			Map<String, ?> flashMap=RequestContextUtils.getInputFlashMap(request);
+		    System.out.println("카카오 로그인==>"+flashMap.get("id"));
+		}*/
+		
 		
 		//로그인수 +1;
 		managerService.logincount();
-		//예약수
+		//예약수  
 		//???
 		
 		//로그인 수 가져오기(현재 로그인 수만)
@@ -36,7 +51,7 @@ public class ManagerMainController {
 	
 	@RequestMapping(value="/loginX.mdo", method=RequestMethod.GET)
 	public ModelAndView logincheck() {
-		
+		System.out.println("로그인 화면으로 이동");
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("body/loginX");
 		
--- Luna/src/main/java/com/study/luna/mg/controller/MgSalesController.java
+++ Luna/src/main/java/com/study/luna/mg/controller/MgSalesController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,107 +20,112 @@ import com.study.luna.mg.model.BoardPager;
 import com.study.luna.mg.model.SalesVO;
 import com.study.luna.mg.service.MgPCService;
 import com.study.luna.mg.util.excelView;
+import com.study.luna.mg.util.listExcelDownload;
 
 @Controller
 public class MgSalesController {
-	//현황보기 
-	
-		@Autowired
-		private MgPCService MgPCService;
-		
-		//매출현황
-		@RequestMapping(value="/mgSales.mdo")
-		public ModelAndView mgSalesView(SalesVO vo,
-				@RequestParam(defaultValue="")String keyword,
-				@RequestParam(defaultValue="1")int curPage) throws Exception{
-			
-			//레코드계산
-			int count = MgPCService.countArticle(keyword); 
-			
-			System.out.println("키워드 :"+keyword.toString());
-			
-			//페이지 나누기처리
-			BoardPager boardPager= new BoardPager(count,curPage);
-			int start = boardPager.getPageBegin(); 
-			int end = boardPager.getPageEnd(); 
-			
-			List<SalesVO> list = MgPCService.SalesList(start,end,keyword);
-			
-			Map<String, Object> map = new HashMap<String,Object>();
-			map.put("list", list); 
-			map.put("count", count);
-			map.put("keyword", keyword);
-			map.put("boardPager", boardPager);
-			
-			ModelAndView mv = new ModelAndView();
-			mv.setViewName("body/presentCondition/mgSales");
-			mv.addObject("map",map);
-			
-			System.out.println("Mg_sales 컨트롤러");
-			return mv;
-		}
-		
-		//매출현황 excel 다운로드
-		@RequestMapping(value="/excelDownload.mdo")
-		public  View excelTransform(Map<String, Object> modelmap,Model model,SalesVO vo,HttpServletResponse response) throws Exception{
-		/*	
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("application/vnd.ms-excel");
-			 response.setHeader("Pragma","public");
-	         response.setHeader("Expires","0");
-	         response.setHeader("Content-Disposition","attachment; filename = test.xls");*/
+	// 현황보기
 
-	         //List<Object> excelList = MgPCService.excelList(vo);
-	         //List<SalesVO> list = MgPCService.SalesList(vo);
-	       //파일변환 타겟 얻어오기
+	@Autowired
+	private MgPCService MgPCService;
+
+	// 매출현황
+	@RequestMapping(value = "/mgSales.mdo")
+	public ModelAndView mgSalesView(SalesVO vo, @RequestParam(defaultValue = "") String keyword,
+			@RequestParam(defaultValue = "1") int curPage) throws Exception {
+
+		// 레코드계산
+		int count = MgPCService.countArticle(keyword);
+
+		System.out.println("키워드 :" + keyword.toString());
+
+		// 페이지 나누기처리
+		BoardPager boardPager = new BoardPager(count, curPage);
+		int start = boardPager.getPageBegin();
+		int end = boardPager.getPageEnd();
+
+		List<SalesVO> list = MgPCService.SalesList(start, end, keyword);
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("list", list);
+		map.put("count", count);
+		map.put("keyword", keyword);
+		map.put("boardPager", boardPager);
+
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("body/presentCondition/mgSales");
+		mv.addObject("map", map);
+
+		System.out.println("Mg_sales 컨트롤러");
+		return mv;
+	}
+
+	// 매출현황 excel 다운로드
+/*	@RequestMapping(value = "/excelDownload.mdo")
+	public View excelTransform(Map<String, Object> modelmap, Model model, SalesVO vo, HttpServletResponse response)
+			throws Exception {
+		
+		 * response.setCharacterEncoding("UTF-8");
+		 * response.setContentType("application/vnd.ms-excel");
+		 * response.setHeader("Pragma","public"); response.setHeader("Expires","0");
+		 * response.setHeader("Content-Disposition","attachment; filename = test.xls");
+		 
+
+		// List<Object> excelList = MgPCService.excelList(vo);
+		// List<SalesVO> list = MgPCService.SalesList(vo);
+		// 파일변환 타겟 얻어오기
 //	 		String type = model.get("Mgsales").toString();
-	       //  model.addAttribute("list", list);
-			return new excelView();
+		// model.addAttribute("list", list);
+		return new excelView();
 
-	 	/*	model.put("excelList",excelList); 
-	 		model.put("excelType",type);
-	*/
+		
+		 * model.put("excelList",excelList); model.put("excelType",type);
+		 
 //	         return  new ModelAndView("excelView",model);    //"excelView";
-			
-			
-			
-			
-			/*//타켓이 안구해짐 링크랑 관련이있는것같은데
-			String target = paramMap.get("target").toString();
-			response.setHeader("content-disposition", "attachment; filename=매출현황.xmlx");
-			List<Object> excelList = MgPCService.getExcelObject(paramMap);
-			ModelMap.put("excelList", excelList);
-			ModelMap.put("target", target);
-			
-			System.out.println("엑셀다운링크");
-			return "excelView";*/
-		}
+
 		
-		
-		//예약현황
-		@RequestMapping(value="/mgReserve.mdo")
-		public ModelAndView mgReserveView(SalesVO vo) {
-			
-			
-			List<SalesVO> list = MgPCService.mgReserveList(vo);
-			
-			ModelAndView mv = new ModelAndView();
-			mv.addObject("list",list);
-			mv.setViewName("body/presentCondition/mgReserve");
-			
-			
-			return mv;
-		}
-		
-		//방문현황
-		@RequestMapping(value="/mgVisit.mdo",method=RequestMethod.GET)
-		public String mgVisitView() {
-			return "body/presentCondition/mgVisit";
-		}
-		
-		//
-		@RequestMapping(value="/mgRefund.mdo",method=RequestMethod.GET)
-		public String mgRefundView() {
-			return "body/presentCondition/mgRefund";
-		}
+		 * //타켓이 안구해짐 링크랑 관련이있는것같은데 String target = paramMap.get("target").toString();
+		 * response.setHeader("content-disposition", "attachment; filename=매출현황.xmlx");
+		 * List<Object> excelList = MgPCService.getExcelObject(paramMap);
+		 * ModelMap.put("excelList", excelList); ModelMap.put("target", target);
+		 * 
+		 * System.out.println("엑셀다운링크"); return "excelView";
+		 
+	}*/
+
+	@RequestMapping(value = "/excelDownload.mdo")
+	public View excelDownload(HttpServletRequest request, HttpServletResponse response, Model model, SalesVO vo)
+			throws Exception {
+
+		List<SalesVO> list = MgPCService.mgReserveList(vo);
+
+		model.addAttribute("list", list);
+
+		return new listExcelDownload();
+	}
+
+	// 예약현황
+	@RequestMapping(value = "/mgReserve.mdo")
+	public ModelAndView mgReserveView(SalesVO vo) {
+
+		List<SalesVO> list = MgPCService.mgReserveList(vo);
+
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("list", list);
+		mv.setViewName("body/presentCondition/mgReserve");
+
+		return mv;
+	}
+
+	// 방문현황
+	@RequestMapping(value = "/mgVisit.mdo", method = RequestMethod.GET)
+	public String mgVisitView() {
+		return "body/presentCondition/mgVisit";
+	}
+
+	//
+	@RequestMapping(value = "/mgRefund.mdo", method = RequestMethod.GET)
+	public String mgRefundView() {
+		return "body/presentCondition/mgRefund";
+	}
 }
